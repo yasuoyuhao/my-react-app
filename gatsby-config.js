c67701655bb98328891a7ac600a3f636cdd98680
yuhaoyasuo <yasuoyuhao@gmail.com>
2019-04-07 14:29:37 +0800

    # remode api
    # fix and can run, show

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,5 @@ module.exports = {
 	keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: '0ge8xzmnbp2c',
-        accessToken: 'b3b275d4fda32085546b9100cb9ae7cb8796fdddd462fd42ba53c4c17bf2f99d'
-      }
-    }
   ],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,16 +8,9 @@ import Footer from '../components/Footer';
 
 const Layout = ({ children, data }) => (
   <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: data.site.siteMetadata.description },
-        { name: 'keywords', content: data.site.siteMetadata.keywords },
-      ]}
-    />
     <Header />
     {children()}
-    <Footer data={data}>
+    <Footer>
       Backgrounds made in Cinema 4D, iOS app in Swift, site in React. <a href="mailto:support@designcode.io">Email us</a> to ask anything. © 2018
     </Footer>
   </div>
@@ -27,25 +20,4 @@ Layout.propTypes = {
   children: PropTypes.func,
 }
 
-export default Layout
-
-export const query = graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-        description
-        keywords
-      }
-    }
-    allContentfulLink(sort: { fields: [createdAt], order: ASC }) {
-      edges {
-        node {
-          title
-          url
-          createdAt
-        }
-      }
-    }
-  }
-`
+export default Layout;
